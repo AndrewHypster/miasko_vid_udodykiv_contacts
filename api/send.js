9e7afe8297028c38381fe46d5921b7514b4334cc
AndrewHypster <andrewgrechukh123@gmail.com>
2025-05-15 13:27:15 +0300

    add form

--- api/send.js
+++ api/send.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-module.exports = async (req, res) => {
+export default async function handler (req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
